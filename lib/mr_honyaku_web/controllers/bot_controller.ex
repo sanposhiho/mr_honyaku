b5610bba65321b3bacc64b6a08485007790a74f3
Kensei Nakada <handbomusic@icloud.com>
2019-12-03 11:08:26 +0900

    complete

--- lib/mr_honyaku_web/controllers/bot_controller.ex
+++ lib/mr_honyaku_web/controllers/bot_controller.ex
@@ -1,29 +1,24 @@
 defmodule MrHonyakuWeb.BotController do
   use MrHonyakuWeb, :controller
 
-  def get_image(conn, %{"message_id" => message_id}) do
-    IO.puts("oKKKK")
-    image_url = File.read!("#{message_id}.jpg")
-    IO.puts("oKKKK")
-    send_resp(conn, 200, image_url)
-  end
   def line_callback(conn, %{"events" => events}) do
     %{"message" => message } = List.first(events)
     %{"source" => source } = List.first(events)
     events = List.first(events)
     endpoint_uri = "https://api.line.me/v2/bot/message/reply"
+    line_auth_token = Application.get_env(:mr_honyaku, :line_auth_token)
 
     json_data =
     case message["type"] do
       "image" ->
         image_url = "https://api.line.me/v2/bot/message/#{message["id"]}/content"
-        header = %{"Authorization" => "Bearer ${7qco1iW1oMODOe/GL9HtBqxxaPvayqwpnABfUJ7pgYlp0yCCX6gyAHLwQhIRXk9Yyu2wGMguVX7JmaKjlf9DiAQHF2xtbWbpf35DcR1HSQY/gTBozEyw0IPZMdvWjERc9NuSjvVffB6JxF7URfYkZwdB04t89/1O/w1cDnyilFU=}"}
+        header = %{"Authorization" => "Bearer ${#{line_auth_token}}"}
         %HTTPoison.Response{body: body} = HTTPoison.get!(image_url, header)
-        File.write!("#{message["id"]}.jpg", body)
+        image = body |> Base.encode64()
         brain_url =  "https://ocr-devday19.linebrain.ai/v1/recognition"
-        service_id = "wUjIzhWuLOsDMOU5GMJ2XdMYNCfukH7E"
+        service_id = Application.get_env(:mr_honyaku, :brain_service_id)
         data = %{
-          imageURL: ["https://salty-anchorage-80672.herokuapp.com/#{message["id"]}"],
+          imageContent: [image],
                  entrance: "detection",
                  scaling: false,
                  segments: false
@@ -33,34 +28,40 @@ defmodule MrHonyakuWeb.BotController do
                     "X-ClovaOCR-Service-ID" => service_id,
                     "Content-Type" => "application/json"
                   }
-        json_data =
         case HTTPoison.post(brain_url, data, headers) do
           {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
             IO.puts body
-            data = Poison.decode!(data)
-            words = data["words"]
+            image_data = Poison.decode!(body)
+            words = image_data["words"]
             text = Enum.map(words, fn word -> word["text"] end)
-            messages = Enum.map(text, fn raw_text ->
-              %{
-                raw: raw_text,
-                translated: translate("ja", "en", raw_text)
-              }
-            end)
-            |> Enum.map(fn texts ->
-                [
+            raw_text = Enum.join(text)
+            messages =
+              case translate("ja", "en", raw_text) do
+                {:ok, translated} ->
                   %{
-                    type: "text",
-                    text: "元の文章："<>texts[:raw]
-                  },
-                  %{
-                    type: "text",
-                    text: "翻訳："<>texts[:translated]
+                    raw: raw_text,
+                    translated: translated
                   }
-                ]
-                end)
+                _ ->
+                  %{
+                    raw: raw_text,
+                    translated: "エラーが発生しました。。"
+                  }
+              end
+            messages_list=
+            [
+              %{
+                type: "text",
+                text: "原文："<>messages[:raw]
+              },
+              %{
+                type: "text",
+                text: "翻訳："<>messages[:translated]
+              }
+            ]
             |> List.flatten()
             %{replyToken: events["replyToken"],
-                messages: messages
+                messages: messages_list
             } |> Poison.encode!
 
           error ->
@@ -80,15 +81,16 @@ defmodule MrHonyakuWeb.BotController do
             messages: [
               %{
               type: "text",
-              text: "画像を送ってね！" # 受信したメッセージをそのまま返す
+              text: "画像を送ってね！"
               }
             ]
           } |> Poison.encode!
     end
 
+
     headers = %{
       "Content-Type" => "application/json",
-      "Authorization" => "Bearer ${7qco1iW1oMODOe/GL9HtBqxxaPvayqwpnABfUJ7pgYlp0yCCX6gyAHLwQhIRXk9Yyu2wGMguVX7JmaKjlf9DiAQHF2xtbWbpf35DcR1HSQY/gTBozEyw0IPZMdvWjERc9NuSjvVffB6JxF7URfYkZwdB04t89/1O/w1cDnyilFU=}"   #メッセージ送受信設定|>アクセストークンからアクセストークンを取得
+      "Authorization" => "Bearer ${#{line_auth_token}}"
     }
 
     case HTTPoison.post(endpoint_uri, json_data, headers) do
@@ -104,11 +106,35 @@ defmodule MrHonyakuWeb.BotController do
   end
 
   def translate(source, target, text) do
-    url = "https://script.google.com/macros/s/AKfycbztT9dreJnvkAQclPEONs27RwkKGdUt7dKDitIRQ08ppJeNt5Bm/exec?text=#{text}&source=#{source}&target=#{target}"
-    case HTTPoison.get(url) do
-      {:ok, response} -> translated_text = Poison.decode(response)
-                         translated_text["text"]
-      {:error, reason} -> {:error, reason}
+    url = "https://mr-honyaku.cognitiveservices.azure.com/sts/v1.0/issuetoken"
+    translation_auth = Application.get_env(:mr_honyaku, :translation_auth)
+    headers = %{"Ocp-Apim-Subscription-Key" => translation_auth }
+    with %HTTPoison.Response{status_code: 200, body: body} <- HTTPoison.post!(url, [], headers) do
+      IO.puts body
+      token = "Bearer "<>body
+      transrate_url = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=#{target}"
+      headers = %{
+        "Authorization" => token,
+        "Content-Type" => "application/json; charset=UTF-8",
+        "Content-Length" => String.length(text)
+      }
+      json = [%{
+        "text" => text,
+      }]|>Poison.encode!
+      case HTTPoison.post(transrate_url, json, headers) do
+        {:ok,%HTTPoison.Response{status_code: 200, body: body}}->
+          IO.inspect body
+          body = body |> Poison.decode!|>Enum.at(0)
+          translations = body["translations"]|>Enum.at(0)
+          {:ok, translations["text"]}
+        error ->
+          IO.inspect error
+          {:error, error}
+      end
+
+    else
+      error -> IO.inspect error
+        {:error, error}
     end
   end
 end
